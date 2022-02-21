3650fc0f5cd20055f9dc5ae31704254e85e4a979
BIGEL Thomas <thomas.bigel4@etu.univ-lorraine.fr>
2022-02-21 17:55:04 +0100

    Lesson 4

--- README.md
+++ README.md
@@ -25,5 +25,5 @@ How to use: ./main (+optional path to model (default used: obj/african_head.obj)
 ```bash
 ./main
 ```
-![image output](https://github.com/thomasbigel/CoursSokolovMoteur3D/blob/readmeImage/Lesson3Texture/output.png)
+![image output](https://github.com/thomasbigel/CoursSokolovMoteur3D/blob/readmeImage/Lesson4/output.png)
 
--- main.cpp
+++ main.cpp
@@ -13,8 +13,36 @@ const TGAColor green = TGAColor(0,   255, 0);
 const TGAColor blue  = TGAColor(0,   0,   255);
 const int width  = 800; 
 const int height = 800; 
+const int depth  = 255;
 Model *model = NULL;
 float *zbuffer = new float[width*height];
+
+Vec3f camera(0,0,3);
+
+Vec3f m2v(Matrix m) {
+    return Vec3f(m[0][0]/m[3][0], m[1][0]/m[3][0], m[2][0]/m[3][0]);
+}
+
+Matrix v2m(Vec3f v) {
+    Matrix m(4, 1);
+    m[0][0] = v.x;
+    m[1][0] = v.y;
+    m[2][0] = v.z;
+    m[3][0] = 1.f;
+    return m;
+}
+
+Matrix viewport(int x, int y, int w, int h) {
+    Matrix m = Matrix::identity(4);
+    m[0][3] = x+w/2.f;
+    m[1][3] = y+h/2.f;
+    m[2][3] = depth/2.f;
+
+    m[0][0] = w/2.f;
+    m[1][1] = h/2.f;
+    m[2][2] = depth/2.f;
+    return m;
+}
 
 void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor &color) { 
     bool steep = false; 
@@ -140,6 +168,10 @@ int main(int argc, char** argv) {
 
     for (int i=width*height; i--; zbuffer[i] = -std::numeric_limits<float>::max());
 
+    Matrix Projection = Matrix::identity(4);
+    Matrix ViewPort   = viewport(width/8, height/8, width*3/4, height*3/4);
+    Projection[3][2] = -1.f/camera.z;
+
     TGAImage image(width, height, TGAImage::RGB);
     Vec3f light_dir(0,0,-1);
     for (int i=0; i<model->nfaces(); i++) {
@@ -148,7 +180,8 @@ int main(int argc, char** argv) {
         Vec3f world_coords[3];
         for (int j=0; j<3; j++) {
             Vec3f v = model->vert(face[j]);
-            screen_coords[j] = Vec3f(int((v.x+1.)*width/2.+.5), int((v.y+1.)*height/2.+.5), v.z);
+            //screen_coords[j] = Vec3f(int((v.x+1.)*width/2.+.5), int((v.y+1.)*height/2.+.5), v.z);
+            screen_coords[j] =  m2v(ViewPort*Projection*v2m(v));
             world_coords[j]  = v;
         }
         Vec3f n = (world_coords[2]-world_coords[0])^(world_coords[1]-world_coords[0]);
@@ -165,7 +198,18 @@ int main(int argc, char** argv) {
 
     image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
     image.write_tga_file("output.tga");
+
+        TGAImage zbimage(width, height, TGAImage::GRAYSCALE);
+        for (int i=0; i<width; i++) {
+            for (int j=0; j<height; j++) {
+                zbimage.set(i, j, TGAColor(zbuffer[i+j*width], 1));
+            }
+        }
+        zbimage.flip_vertically(); // i want to have the origin at the left bottom corner of the image
+        zbimage.write_tga_file("zbuffer.tga");
+
     delete model;
+    delete [] zbuffer;
 
 /* 2D
 // just dumping the 2d scene (yay we have enough dimensions!)
