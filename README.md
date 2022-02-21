8abc72a2a3e0e172ffd7b3ddf750061b1bee3aae
BIGEL Thomas <thomas.bigel4@etu.univ-lorraine.fr>
2022-02-21 17:51:33 +0100

    Lesson 3

--- README.md
+++ README.md
@@ -25,5 +25,5 @@ How to use: ./main (+optional path to model (default used: obj/african_head.obj)
 ```bash
 ./main
 ```
-![image output](https://github.com/thomasbigel/CoursSokolovMoteur3D/blob/readmeImage/Lesson2/output.png)
+![image output](https://github.com/thomasbigel/CoursSokolovMoteur3D/blob/readmeImage/Lesson3/output.png)
 
--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <vector>
 #include <cmath>
+#include <limits>
 #include "tgaimage.h"
 #include "model.h"
 #include "geometry.h"
@@ -7,11 +8,13 @@
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red   = TGAColor(255, 0,   0,   255);
 const TGAColor green = TGAColor(0,   255, 0,   255);
+const TGAColor blue  = TGAColor(0,   0,   255, 255);
 const int width  = 800; 
 const int height = 800; 
 Model *model = NULL;
+float *zbuffer = new float[width*height];
 
-void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) { 
+void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor &color) { 
     bool steep = false; 
     if (std::abs(x0-x1)<std::abs(y0-y1)) { 
         std::swap(x0, y0); 
@@ -41,7 +44,12 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
     } 
 } 
 
-Vec3f barycentric(Vec2i *pts, Vec2i P) { 
+void lineVec(Vec2i p0, Vec2i p1, TGAImage &image, TGAColor color) { 
+    //line(p0->x, p0->y, p1->x, p1->y, image, color);
+    line(p0.x, p0.y, p1.x, p1.y, image, color);
+}
+
+Vec3f barycentric(Vec3f *pts, Vec3f P) { 
     Vec3f u = crossGeo(Vec3f(pts[2][0]-pts[0][0], pts[1][0]-pts[0][0], pts[0][0]-P[0]), Vec3f(pts[2][1]-pts[0][1], pts[1][1]-pts[0][1], pts[0][1]-P[1]));
     /* `pts` and `P` has integer value as coordinates
        so `abs(u[2])` < 1 means `u[2]` is 0, that means
@@ -50,24 +58,46 @@ Vec3f barycentric(Vec2i *pts, Vec2i P) {
     return Vec3f(1.f-(u.x+u.y)/u.z, u.y/u.z, u.x/u.z); 
 } 
 
-void triangle(Vec2i *pts, TGAImage &image, TGAColor color) {
-    Vec2i bboxmin(image.get_width()-1,  image.get_height()-1); 
-    Vec2i bboxmax(0, 0); 
-    Vec2i clamp(image.get_width()-1, image.get_height()-1); 
+void triangle(Vec3f *pts, TGAImage &image, TGAColor color) {
+    Vec2f bboxmin(image.get_width()-1,  image.get_height()-1); 
+    Vec2f bboxmax(0, 0); 
+    Vec2f clamp(image.get_width()-1, image.get_height()-1); 
     for (int i=0; i<3; i++) { 
         for (int j=0; j<2; j++) { 
-            bboxmin[j] = std::max(0,        std::min(bboxmin[j], pts[i][j])); 
+            bboxmin[j] = std::max(0.f,        std::min(bboxmin[j], pts[i][j])); 
             bboxmax[j] = std::min(clamp[j], std::max(bboxmax[j], pts[i][j])); 
         } 
     } 
-    Vec2i P; 
+    Vec3f P; 
     //loop sur les points à l'interieur de la box 2D du triangle
     
     for (P.x=bboxmin.x; P.x<=bboxmax.x; P.x++) { 
         for (P.y=bboxmin.y; P.y<=bboxmax.y; P.y++) { 
             Vec3f bary_point = barycentric(pts, P);
             if (bary_point.x<0 || bary_point.y<0 || bary_point.z<0) continue; 
-            image.set(P.x, P.y, color);
+            P.z = 0;
+            for(int i=0; i<3; i++){
+                P.z += pts[i][2] * bary_point[i];
+            }
+            if(zbuffer[int(P.x + P.y * width)] < P.z){
+                zbuffer[int(P.x + P.y * width)] = P.z;
+                image.set(P.x, P.y, color);
+            }
+
+        }
+    }
+}
+
+void rasterize(Vec2i p0, Vec2i p1, TGAImage &image, TGAColor color, int ybuffer[]) {
+    if (p0.x>p1.x) {
+        std::swap(p0, p1);
+    }
+    for (int x=p0.x; x<=p1.x; x++) {
+        float t = (x-p0.x)/(float)(p1.x-p0.x);
+        int y = p0.y*(1.-t) + p1.y*t;
+        if (ybuffer[x]<y) {
+            ybuffer[x] = y;
+            image.set(x, 0, color);
         }
     }
 }
@@ -79,15 +109,17 @@ int main(int argc, char** argv) {
         model = new Model("obj/african_head.obj");
     }
 
+    for (int i=width*height; i--; zbuffer[i] = -std::numeric_limits<float>::max());
+
     TGAImage image(width, height, TGAImage::RGB);
     Vec3f light_dir(0,0,-1);
     for (int i=0; i<model->nfaces(); i++) {
         std::vector<int> face = model->face(i);
-        Vec2i screen_coords[3];
+        Vec3f screen_coords[3];
         Vec3f world_coords[3];
         for (int j=0; j<3; j++) {
             Vec3f v = model->vert(face[j]);
-            screen_coords[j] = Vec2i((v.x+1.)*width/2., (v.y+1.)*height/2.);
+            screen_coords[j] = Vec3f(int((v.x+1.)*width/2.+.5), int((v.y+1.)*height/2.+.5), v.z);
             world_coords[j]  = v;
         }
         Vec3f n = (world_coords[2]-world_coords[0])^(world_coords[1]-world_coords[0]);
@@ -101,4 +133,41 @@ int main(int argc, char** argv) {
     image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
     image.write_tga_file("output.tga");
     delete model;
+
+/* 2D
+// just dumping the 2d scene (yay we have enough dimensions!)
+    TGAImage scene(width, height, TGAImage::RGB);
+
+    // scene "2d mesh"
+    lineVec(Vec2i(20, 34),   Vec2i(744, 400), scene, red);
+    lineVec(Vec2i(120, 434), Vec2i(444, 400), scene, green);
+    lineVec(Vec2i(330, 463), Vec2i(594, 200), scene, blue);
+
+    // screen line
+    lineVec(Vec2i(10, 10), Vec2i(790, 10), scene, white);
+
+    scene.flip_vertically(); // i want to have the origin at the left bottom corner of the image
+    scene.write_tga_file("scene.tga");
+
+    TGAImage render(width, 16, TGAImage::RGB);
+    int ybuffer[width];
+    for (int i=0; i<width; i++) {
+         ybuffer[i] = std::numeric_limits<int>::min();
+     }
+    rasterize(Vec2i(20, 34),   Vec2i(744, 400), render, red,   ybuffer);
+    rasterize(Vec2i(120, 434), Vec2i(444, 400), render, green, ybuffer);
+    rasterize(Vec2i(330, 463), Vec2i(594, 200), render, blue,  ybuffer);
+
+    // 1-pixel wide image is bad for eyes, lets widen it
+    for (int i=0; i<width; i++) {
+        for (int j=1; j<16; j++) {
+            render.set(i, j, render.get(i, 0));
+        }
+    }
+    render.flip_vertically(); // i want to have the origin at the left bottom corner of the image
+    render.write_tga_file("render.tga");
+*/
+
+
+    return 0;
 }
